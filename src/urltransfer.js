96671bc2bda86075660693b906e9ef4157f80736
luckyapple <larrie.wang@gmail.com>
2012-11-29 14:46:40 +0800

    use boostrap

--- src/urltransfer.js
+++ src/urltransfer.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
-	var charUrl = 'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chld=L|4&chs=200x200&chl=';//Hello,world
+	var charUrl = 'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chld=L|1&chs=200x200&chl=';
 	
 	var generateQrCode = function(){
-		var content = $('#url_id').val();
+		var content = $('#target_id').val();
+		console.log(content);
 		if(content.length == 0)
 			return;
-		var imgDiv = '<img src="' + charUrl + content + '" style="border:solid #eeeeee 1px;padding:2px;"/>';
+		var imgDiv = '<img src="' + charUrl + content + '"width="200" height="200" style="border:solid #eeeeee 1px;padding:2px;"/>';
 		$('#qrcode_container').empty().append(imgDiv);
 	}
 	
-	$("input[type='text']").bind('change', function() {
+	$("#ok_btn_id").bind('click', function() {
 		generateQrCode();
+	});
+	
+	$("#mini_btn_id").bind('click', function(){
+		var textareaHeight = $('#target_id').height();
+		console.log(textareaHeight);
+		
+		if(textareaHeight > 20) {
+			$('#target_id').height(20);
+			$("#mini_btn_id").empty().append('FULL');
+		}
+		else {
+			$('#target_id').height(120);
+			$("#mini_btn_id").empty().append('MINI');
+		}
+		return false;
+	});
+	
+	$("#history_btn_id").bind('click', function(){
+		return false;
 	});
 });
